173edb3e68b85f9ab42ac52472af3350fcde0509
LencoDigitexer <vkonatkte258@gmail.com>
2021-03-04 21:42:14 +0300

    add info from soundcloud

--- extension/popup.js
+++ extension/popup.js
@@ -63,7 +63,7 @@ function send_current(){
             if(status == 200) {
                 const obj = JSON.parse(request.responseText);
                 document.getElementById("test").innerHTML = obj["item"]["name"] + " " + obj["item"]["artists"][0]["name"];
-                
+                get_genre(obj["item"]["name"] + " " + obj["item"]["artists"][0]["name"]);
             }
         }
     }
@@ -76,8 +76,61 @@ function send_current(){
     
     request.onreadystatechange = change_info;
     request.send();
+
     
 }
+
+function get_genre(track_name) {
+    // получаем значение поля key
+    //var keyBox = document.search.key;
+    var val = track_name
+
+    //импорт функции
+    var request = new XMLHttpRequest();
+
+    //вместо div блока output вывести значение ответа
+    function reqReadyStateChange() {
+        if(request.readyState == 4) {
+            var status = request.status;
+            if(status == 200) {
+                //document.getElementById("output").innerHTML = request.responseText;
+                const obj = JSON.parse(request.responseText);
+                //document.getElementById("output").innerHTML = obj[0].id;
+                document.getElementById("genre1").innerHTML = obj[0].genre;
+                document.getElementById("genre2").innerHTML = obj[1].genre;
+                document.getElementById("title1").innerHTML = obj[0].title;
+                document.getElementById("title2").innerHTML = obj[1].title;
+                document.getElementById("user1").innerHTML = obj[0].user.username;
+                document.getElementById("user2").innerHTML = obj[1].user.username;
+                document.getElementById("img1").src=obj[0].artwork_url;
+                document.getElementById("img2").src=obj[0].artwork_url;
+                chrome.tabs.query({active : true, currentWindow: true}, function (tab) {
+                    var activeTabUrl = tab.url;
+                    document.getElementById("tabsurl").innerHTML = tab;
+                    console.log(activeTabUrl);
+                });
+
+                chrome.storage.local.set({'token': 'abc123'}, function() {
+                    console.log('Settings saved');
+                });
+                chrome.storage.local.set(['token'], function(items) {
+                    message('Settings retrieved', items);
+                    document.getElementById("test")  = items;
+                });
+                
+            }
+        }
+    }
+    // строка с параметрами для отправки
+    var body = val;
+    request.open("GET", "https://api.soundcloud.com/tracks?client_id=a25e51780f7f86af0afa91f241d091f8&q=" + body);
+    request.onreadystatechange = reqReadyStateChange;
+    request.send();
+
+    
+    
+}
+
 
 
 
