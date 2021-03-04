d132c77a55bc73f49cc729fcbf1ba72bcf1bbec8
LencoDigitexer <addminuser258@gmail.com>
2021-03-04 23:01:45 +0300

    simpled get token

--- get_token.py
+++ get_token.py
@@ -1,13 +1,9 @@
 import requests
+import webbrowser
 
-headers = {
-    'Authorization': 'Basic ZGU2MTU0NGE2YWRmNDQxNDhhM2FmNzdkZmE0MDViOGU6NTQyNzU5MTljOGZlNGJlNWEwYjQwZmY0ZDEzZjY1ZDI=',
-}
+url = "https://accounts.spotify.com/authorize?client_id=de61544a6adf44148a3af77dfa405b8e&response_type=token&redirect_uri=http://localhost:8888/callback&scope=user-read-currently-playing&state=34fFs29kd09"
+webbrowser.open_new(url)
 
-data = {
-  'grant_type': 'authorization_code',
-  'code': 'AQDKv2ufaMbWd5sMfigK9zoGXdN0WtWVUPI9fTSaSKNsip3qxZ_y78qF7zuNkXXVWQ16dO3d1AqwY1N7a7K3FwtrO5qOXp6ZJ3OGoyDOSgoPwlGpCvsEX-o9q9_j0NHeX-JW6ArFExXluSu1gV-sjPpluxZEZ2LKXD6orxoIGNYrl13oIhc-LkAupn2b374IV5mr3t8nP8oEBTqDEJeIdZHvZqTZEQ',
-  'redirect_uri': 'http://localhost:8888/callback'
-}
+#response = requests.get('https://accounts.spotify.com/authorize?client_id=de61544a6adf44148a3af77dfa405b8e&response_type=token&redirect_uri=http://localhost:8888/callback&scope=user-read-currently-playing&state=34fFs29kd09')
 
-response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
+#print(response.text)
